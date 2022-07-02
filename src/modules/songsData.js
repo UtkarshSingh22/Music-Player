e164a115878399d4e70e3d8e640f5e9f1406fca1
Utkarsh Singh <sutkarsh453@gmail.com>
2022-07-03 01:49:45 +0530

    Add music object data

--- src/modules/songsData.js
+++ src/modules/songsData.js
@@ -1,44 +1,51 @@
 const songList = [
     {
-        name: "",
-        artist: "",
-        imageSrc: "",
-        duration: "",
+        name: "On & On",
+        artist: "Cartoon x HAVSUN x WAYOUT, ft. Daniel Levi",
+        imageSrc: "dist/song-images/on&on.jpg",
+        songSrc:
+            "dist/sample-songs/Cartoon - On & On (ft. Daniel Levi)(Cartoon x HAVSUN x WAYOUT Remix) [NCS Release].mp3",
+        duration: "03:58",
     },
     {
-        name: "",
-        artist: "",
-        imageSrc: "",
-        duration: "",
+        name: "Mortals",
+        artist: "Warriyo, feat. Laura Brehm",
+        imageSrc: "dist/song-images/warriyo.jpg",
+        songSrc:
+            "dist/sample-songs/Warriyo - Mortals (feat. Laura Brehm) [NCS Release].mp3",
+        duration: "03:48",
     },
     {
-        name: "",
-        artist: "",
-        imageSrc: "",
-        duration: "",
+        name: "Cradles",
+        artist: "Sub Urban",
+        imageSrc: "dist/song-images/cradles.jpg",
+        songSrc: "dist/sample-songs/Sub Urban - Cradles [NCS Release].mp3",
+        duration: "03:29",
     },
     {
-        name: "",
-        artist: "",
-        imageSrc: "",
-        duration: "",
+        name: "Adventures",
+        artist: "William Ekh, feat. Alexa Lusader",
+        imageSrc: "dist/song-images/adventures.jpg",
+        songSrc:
+            "dist/sample-songs/William Ekh - Adventures (feat. Alexa Lusader) [NCS Release].mp3",
+        duration: "03:11",
     },
     {
-        name: "",
-        artist: "",
-        imageSrc: "",
-        duration: "",
+        name: "Set Me Free",
+        artist: "Simbai & Frizzy The Streetz",
+        imageSrc: "dist/song-images/setmefree.jpg",
+        songSrc:
+            "dist/sample-songs/Simbai & Frizzy The Streetz - Set Me Free [NCS Release].mp3",
+        duration: "02:49",
     },
     {
-        name: "",
-        artist: "",
-        imageSrc: "",
-        duration: "",
-    },
-    {
-        name: "",
-        artist: "",
-        imageSrc: "",
-        duration: "",
+        name: "Heroes Tonight",
+        artist: "Janji, ft.Johnning",
+        imageSrc: "dist/song-images/janji.jpg",
+        songSrc:
+            "dist/sample-songs/Janji - Heroes Tonight (feat. Johnning) [NCS Release].mp3",
+        duration: "03:28",
     },
 ];
+
+export default songList;
