5bc0a04b4b481fea971336813ecdbf827ffa5125
Utkarsh Singh <sutkarsh453@gmail.com>
2022-07-04 02:08:52 +0530

    get song index to be played

--- src/modules/User Interface/homeUI.js
+++ src/modules/User Interface/homeUI.js
@@ -1,8 +1,17 @@
 import loadMusicPage from "../musicPage";
+import songList from "../SongsList/songsData";
 
 const getClicked = (e) => {
-    console.log(e.target.className);
-    if (e.target.className == "card-img") {
+    if (e.target.parentElement.className == "cards") {
+        const songName = e.target.parentElement.children[1].textContent;
+
+        let songIndex;
+        for (let i = 0; i < songList.length; i++) {
+            if (songList[i].name == songName) {
+                songIndex = i;
+            }
+        }
+        //TODO: Play music according to index
         loadMusicPage();
     } else {
         return;
