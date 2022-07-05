b46f99fe9bdb134b749cece38fb09255433cd273
Utkarsh Singh <sutkarsh453@gmail.com>
2022-07-06 02:06:59 +0530

    Add search bar results div to home page

--- src/modules/Functionalities/searchBar.js
+++ src/modules/Functionalities/searchBar.js
@@ -3,15 +3,35 @@ import songList from "../SongsList/songsData";
 const getSearchData = () => {
     const searchBar = document.querySelector(".search-bar");
 
-    let searchResults = [];
+    let searchResults = document.createElement("div");
+    searchResults.classList = "resultBox";
+
+    const main = document.querySelector(".main");
 
     searchBar.addEventListener("input", (e) => {
-        console.log(e.target.value);
-        let inputByUser = searchBar.value;
+        let inputByUser = e.target.value;
+        inputByUser = inputByUser.toLowerCase();
 
-        // for(let index = 0; index < songList.length; index++){
+        while (searchResults.firstChild) {
+            searchResults.removeChild(searchResults.firstChild);
+        }
 
-        // }
+        for (let index = 0; index < songList.length; index++) {
+            let getName = songList[index].name;
+            getName = getName.toLowerCase();
+
+            if (getName.includes(inputByUser)) {
+                let matchedStr = document.createElement("div");
+                matchedStr.classList = "matchedStr";
+
+                matchedStr.textContent = songList[index].name;
+                matchedStr.textContent += songList[index].artist;
+
+                searchResults.appendChild(matchedStr);
+
+                main.appendChild(searchResults);
+            }
+        }
     });
 };
 
