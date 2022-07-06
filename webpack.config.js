d426399b47ed8000e0001962ced2f281e230d7a9
Utkarsh Singh <sutkarsh453@gmail.com>
2022-07-07 04:36:47 +0530

    Start from beginning

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require("path");
 module.exports = {
     mode: "production",
     entry: "./src/index.js",
-    devtool: "inline-source-map",
+    devtool: false,
+    performance: {
+        hints: false,
+    },
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
