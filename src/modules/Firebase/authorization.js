0116a8dbd6dbc34b68b960e1623b785df98fa254
Utkarsh Singh <sutkarsh453@gmail.com>
2022-07-13 08:23:50 +0530

    Add functions for auth

--- src/modules/Firebase/authorization.js
+++ src/modules/Firebase/authorization.js
@@ -32,14 +32,50 @@ function isUserSignedIn() {
     return !!getAuth().currentUser;
 }
 
+function authStateObserver(user) {
+    const name = document.querySelector(".userName");
+    const userPicture = document.querySelector(".userPic");
+
+    if (user) {
+        let profilePicUrl = getProfilePicUrl();
+        let userName = getUserName();
+
+        userPicture.style.backgroundImage =
+            "url(" + addSizeToGoogleProfilePic(profilePicUrl) + ")";
+        name.textContent = userName;
+
+        name.removeAttribute("hidden");
+        userPicture.removeAttribute("hidden");
+        signOutBtn.removeAttribute("hidden");
+
+        signInBtn.setAttribute("hidden", "true");
+    } else {
+        name.setAttribute("hidden", "true");
+        userPicture.setAttribute("hidden", "true");
+        signOutBtn.setAttribute("hidden", "true");
+
+        signInBtn.removeAttribute("hidden");
+    }
+}
+
+function addSizeToGoogleProfilePic(url) {
+    if (
+        url.indexOf("googleusercontent.com") !== -1 &&
+        url.indexOf("?") === -1
+    ) {
+        return url + "?sz=150";
+    }
+    return url;
+}
+
 const addAuth = () => {
     const signInBtn = document.querySelector(".signIn");
     const signOutBtn = document.querySelector(".signOut");
-    const userName = document.querySelector(".userName");
-    const userPic = document.querySelector(".userPic");
 
     signInBtn.addEventListener("click", signIn);
     signOutBtn.addEventListener("click", signOutUser);
+
+    initFirebaseAuth();
 };
 
 export default addAuth;
