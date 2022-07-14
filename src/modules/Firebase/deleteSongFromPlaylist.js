a73294e0548e85a1db17e4175993ef8d153b2c1a
Utkarsh Singh <sutkarsh453@gmail.com>
2022-07-14 07:27:17 +0530

    Add delete functionality from playlist page

--- src/modules/Firebase/deleteSongFromPlaylist.js
+++ src/modules/Firebase/deleteSongFromPlaylist.js
@@ -1,39 +1,26 @@
 import { getAuth } from "firebase/auth";
-import {
-    collection,
-    deleteDoc,
-    doc,
-    getDocs,
-    getFirestore,
-} from "firebase/firestore";
+import { deleteDoc, doc, getFirestore } from "firebase/firestore";
 import { app } from "./firebase-config";
+import loadPlaylist from "../playlistPage";
 
-const delSong = async (songName) => {
-    const userUid = getAuth().currentUser.uid;
-
-    const querySnapshot = await getDocs(collection(getFirestore(app), userUid));
-    querySnapshot.forEach((docs) => {
-        if (docs.data().name == songName) {
-            await deleteDoc(doc(getFirestore(app), userUid, songName));
-            console.log('deleted')
-        }
-    });
-}
-
-const deleteSongFromPlaylist = () => {
+const deleteSongFromPlaylist = async () => {
     const delBtns = document.querySelectorAll(".remBtn");
-
-    let songName;
+    const userUid = getAuth().currentUser.uid;
 
     delBtns.forEach((btn) => {
         btn.addEventListener("click", (e) => {
-            songName =
+            let songName =
                 e.srcElement.parentElement.children[0].children[1].children[0]
                     .textContent;
+            try {
+                deleteDoc(doc(getFirestore(app), userUid, songName));
+            } catch (error) {
+                alert(error);
+            }
+
+            loadPlaylist();
         });
     });
-
-    delSong(songName);
 };
 
 export default deleteSongFromPlaylist;
